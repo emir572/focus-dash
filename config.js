ff05eb61e9171fae2df255acf40a79d37a5fbe9f
Emir <hasnemir174@gmail.com>
2026-03-15 23:20:55 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// Bu dosya SENİN ÖZEL ANAHTARLARIN. GitHub'a atma!
+const firebaseConfig = {
+    apiKey: "AIzaSyB1ZCY04CLLzu38N2pc9kkd6q7QpsmOxIc",
+    authDomain: "focus-dash.firebaseapp.com",
+    projectId: "focus-dash",
+    storageBucket: "focus-dash.firebasestorage.app",
+    messagingSenderId: "65658193859",
+    appId: "1:65658193859:web:daeed7bf60f7b85f2ddf76",
+    measurementId: "G-TE28BVCP7K"
+};
